67967a77e1f04027efc23849031ecf15f43a58a0
msilis <mikssilis@gmail.com>
2026-07-27 21:57:51 +0100

    add zod and content config

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection } from 'astro:content'
+import { glob } from 'astro/loaders'
+import z from 'zod'
+
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md, mdx}', base: './src/content/blog' }),
+    schema: z.object({
+        layout: z.string().optional(),
+        title: z.string(),
+        date: z.coerce.date(),
+    }),
+})
+
+export const collections = { blog }
