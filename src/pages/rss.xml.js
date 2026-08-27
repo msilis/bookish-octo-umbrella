e76d1edce315a5d6a9ef778cd7eaaad1270b0e34
msilis <mikssilis@gmail.com>
2026-08-27 20:47:57 +0100

    add rss file

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,16 @@
 import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
-export function GET(context) {
+export async function GET(context) {
+    const items = await getCollection('blog')
     return rss({
         title: 'Miks Silis Blog',
         description: 'Frontend engineering thoughts and observations',
         site: context.site,
-        items: [],
+        items: items.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.date,
+            link: `/blog/${post.id}`,
+        })),
     })
 }
